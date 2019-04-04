d7eb8e2f065b8a7c9eeb8f0c2319a6c34ad516a1
athira-k-athi <39617436+athira-k-athi@users.noreply.github.com>
2019-04-04 15:16:46 +0530

    Update bch_gen.ino

--- bch_gen/bch_gen.ino
+++ bch_gen/bch_gen.ino
@@ -107,7 +107,8 @@ void bch_gen( bool* emg_msg , uint8_t msgLength, uint8_t genLength , uint8_t emg
             Serial.println(genPolynomial[ k - temp ]);
 
             msgPolynomial[ k ] = ( ( msgPolynomial[ k ] ) ^ ( genPolynomial[ k - temp ] ) );
-            
+            Serial.print( k );
+            Serial.print( " " );
             Serial.println(msgPolynomial[ k ]);
           }
         }
